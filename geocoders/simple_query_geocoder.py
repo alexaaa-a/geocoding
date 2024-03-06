2afade0b660b0641e680249c01a528bb5758c42b
alexaaa-a <144144005+alexaaa-a@users.noreply.github.com>
2024-03-06 20:15:00 +0500

    алгоритм "в лоб" реализован

--- geocoders/simple_query_geocoder.py
+++ geocoders/simple_query_geocoder.py
@@ -1,12 +1,18 @@
 from geocoders.geocoder import Geocoder
-
+from api import API
 
 # Алгоритм "в лоб"
 class SimpleQueryGeocoder(Geocoder):
     def _apply_geocoding(self, area_id: str) -> str:
-        """
-            TODO:
-            - Делать запросы к API для каждой area
-            - Для каждого ответа формировать полный адрес
-        """
-        raise NotImplementedError()
+        full_address = ''
+        current_id = area_id
+
+        while current_id is not None:
+            area = API.get_area(current_id)
+            full_address = f"{area.name}, {full_address}"
+            current_id = area.parent_id
+
+        return full_address.strip(", ")
+
+geocoder = SimpleQueryGeocoder(samples=3)
+print(geocoder.geocode())
