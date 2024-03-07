800710b51e994cd802b6a3a0c8d360081bb058a7
alexaaa-a <144144005+alexaaa-a@users.noreply.github.com>
2024-03-08 04:11:27 +0500

    реализован алгоритм "перебор дерева"

--- geocoders/simple_tree_geocoder.py
+++ geocoders/simple_tree_geocoder.py
@@ -12,10 +12,34 @@ class SimpleTreeGeocoder(Geocoder):
             self.__data = data
 
     def _apply_geocoding(self, area_id: str) -> str:
-        """
-            TODO:
-            - Сделать перебор дерева для каждого area_id
-            - В ходе перебора возвращать массив элементов, состоящих из TreeNode необходимой ветки
-            - Из массива TreeNode составить полный адрес
-        """
-        raise NotImplementedError()
+        def find(node: TreeNode, target_id: str):
+            target_id = str(target_id)
+            if node.id == target_id:
+                return node
+
+            else:
+                for child in node.areas:
+                    res = find(child, target_id)
+                    if res is not None:
+                        return res
+            return None
+
+
+        data = self.__data
+        res = []
+        for node in data:
+            result = find(node, area_id)
+            if result is not None:
+                res.append(result.name)
+                a = result.parent_id
+                while a is not None:
+                    b = API.get_area(a)
+                    res.append(b.name)
+                    a = b.parent_id
+        address = ", ".join([node for node in reversed(res)])
+
+        return address
+
+
+geocodd = SimpleTreeGeocoder(samples=5)
+print(geocodd.geocode())
