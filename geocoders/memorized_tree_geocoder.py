9f77088d915f80402dd325ca7639377908e1455a
alexaaa-a <144144005+alexaaa-a@users.noreply.github.com>
2024-03-09 21:59:56 +0500

    реализован алгоритм "мемоизация дерева"

--- geocoders/memorized_tree_geocoder.py
+++ geocoders/memorized_tree_geocoder.py
@@ -11,15 +11,34 @@ class MemorizedTreeGeocoder(Geocoder):
         else:
             self.__data = data
 
-    """
-        TODO:
-        Сделать функцию перебора дерева:
-        - Для каждого узла сохранять в словарь адресов
-    """
+    self.__address: dict[str, str] = {}
 
     def _apply_geocoding(self, area_id: str) -> str:
-        """
-            TODO:
-            - Возвращать данные из словаря с адресами
-        """
-        raise NotImplementedError()
+        def find(node: TreeNode, target_id: str):
+            target_id = str(target_id)
+            if node.id == target_id:
+                return node
+
+            else:
+                for child in node.areas:
+                    res = find(child, target_id)
+                    if res is not None:
+                        return res
+            return None
+
+
+        data = self.__data
+        res = []
+        for node in data:
+            result = find(node, area_id)
+            if result is not None:
+                res.append(result.name)
+                a = result.parent_id
+                while a is not None:
+                    b = API.get_area(a)
+                    res.append(b.name)
+                    a = b.parent_id
+        address = ", ".join([node for node in reversed(res)])
+        self.__address[area_id] = address
+
+        return self.__address[area_id]
