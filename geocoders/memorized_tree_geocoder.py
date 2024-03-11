9ac2cae8404e802f3603ff67e731f3a6114fdd04
alexaaa-a <144144005+alexaaa-a@users.noreply.github.com>
2024-03-11 16:52:54 +0500

    реализован алгоритм "мемоизация дерева"

--- geocoders/memorized_tree_geocoder.py
+++ geocoders/memorized_tree_geocoder.py
@@ -10,35 +10,19 @@ class MemorizedTreeGeocoder(Geocoder):
             self.__data = API.get_areas()
         else:
             self.__data = data
+        self.__addresses = {}
+        self.create_dict(self.__data)
 
-    self.__address: dict[str, str] = {}
+    def create_dict(self, tree: list[TreeNode], parent: str = '') -> dict:
+        for area in tree:
+            if parent:
+                full_address = f'{parent}, {area.name}'
+            else:
+                full_address = area.name
+            self.__addresses[area.id] = full_address
+            self.create_dict(area.areas, full_address)
 
     def _apply_geocoding(self, area_id: str) -> str:
-        def find(node: TreeNode, target_id: str):
-            target_id = str(target_id)
-            if node.id == target_id:
-                return node
+        return self.__addresses.get(str(area_id))
 
-            else:
-                for child in node.areas:
-                    res = find(child, target_id)
-                    if res is not None:
-                        return res
-            return None
-
-
-        data = self.__data
-        res = []
-        for node in data:
-            result = find(node, area_id)
-            if result is not None:
-                res.append(result.name)
-                a = result.parent_id
-                while a is not None:
-                    b = API.get_area(a)
-                    res.append(b.name)
-                    a = b.parent_id
-        address = ", ".join([node for node in reversed(res)])
-        self.__address[area_id] = address
-
-        return self.__address[area_id]
+    
